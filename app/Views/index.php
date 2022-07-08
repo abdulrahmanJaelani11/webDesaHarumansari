f7037efc38e1fb0ddd172c54ea7750bb1cfb28b9
abdurahmanjaelani11 <randikaangga9044@gmail.com>
2022-07-08 09:00:58 +0700

    memperbaiki berita di file index

--- app/Views/index.php
+++ app/Views/index.php
@@ -270,7 +270,7 @@
 
 
     <!-- Team Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s" style="margin-top: 10%;">
         <div class="container py-5" id="aparatDesa">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
                 <h5 class="fw-bold text-primary text-uppercase">Aparatur Desa</h5>
@@ -317,22 +317,26 @@
                     <!-- Blog Detail Start -->
                     <div class="mb-5">
                         <div class="row">
-                            <div class="col-md-12 wow slideInUp" data-wow-delay="0.1s">
-                                <div class="blog-item bg-light rounded overflow-hidden">
-                                    <div class="blog-img position-relative overflow-hidden">
-                                        <img class="img-fluid" src="<?= base_url("assetsAdmin"); ?>/img/<?= $viewBerita['img']; ?>">
-                                        <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">Berita</a>
-                                    </div>
-                                    <div class="p-4">
-                                        <div class="d-flex mb-3">
-                                            <small class="me-3"><i class="far fa-user text-primary me-2"></i><?= $viewBerita['penulis']; ?></small>
-                                            <small><i class="far fa-calendar-alt text-primary me-2"></i><?= $viewBerita['tanggal']; ?></small>
+                            <?php if ($viewBerita['judul'] != '') : ?>
+                                <div class="col-md-12 wow slideInUp" data-wow-delay="0.1s">
+                                    <div class="blog-item bg-light rounded overflow-hidden">
+                                        <div class="blog-img position-relative overflow-hidden">
+                                            <img class="img-fluid" src="<?= base_url("assetsAdmin"); ?>/img/<?= $viewBerita['img']; ?>">
+                                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">Berita</a>
                                         </div>
-                                        <h4 class="mb-3"><?= $viewBerita['judul']; ?></h4>
-                                        <a class="text-uppercase" href="<?= base_url("detail/" . $viewBerita['id']); ?>">Lihat <i class="bi bi-arrow-right"></i></a>
+                                        <div class="p-4">
+                                            <div class="d-flex mb-3">
+                                                <small class="me-3"><i class="far fa-user text-primary me-2"></i><?= $viewBerita['penulis']; ?></small>
+                                                <small><i class="far fa-calendar-alt text-primary me-2"></i><?= $viewBerita['tanggal']; ?></small>
+                                            </div>
+                                            <h4 class="mb-3"><?= $viewBerita['judul']; ?></h4>
+                                            <a class="text-uppercase" href="<?= base_url("detail/" . $viewBerita['id']); ?>">Lihat <i class="bi bi-arrow-right"></i></a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            <?php else : ?>
+                                <h3 class="text-center">Belum Ada Berita Untuk Ditampilkan</h3>
+                            <?php endif; ?>
                         </div>
                     </div>
                     <!-- Blog Detail End -->
